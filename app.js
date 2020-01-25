c611e099366995711d14ee0393142a489021ca43
testuser <joaovictor.clementino@gmail.com>
2020-01-25 00:53:09 -0300

    add api endpoints for moves and items

--- app.js
+++ app.js
@@ -4,40 +4,56 @@ const cors = require("cors")
 const port = process.env.PORT || 8080;
 
 // local in-memory data
-const dexdata = require("./data/dex.json")
-const battledex = {
-    ou: require("./data/gen8ou.json")
+const localDex = {
+    ou: require("./data/gen8ou.json"),
+    items: require("./data/itemdex.json"),
+    moves: require("./data/movedex.json")
 }
 //configurações publicas para dentro do servidor express, adicionando middlewares (body-parser,cors)
-server.use(bodyParser.urlencoded({extended:false}))
+server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
 server.use(cors())
 
-server.get("/:tier/:slug", (req, res)=>{
+server.get("/:key/:slug", (req, res) => {
+    const itemSlug = req.params.slug
+    const key = req.params.key
+    if (key in localDex) {
+        if (itemSlug in localDex[key]) {
+            return res.json(localDex[key][itemSlug])
+        }
+    }
+    return res.json(false)
+})
+
+server.get("/:key", (req, res) => {
+    const key = req.params.key
+    if (key in localDex) {
+        return res.json(localDex[key])
+    }
+    return res.json(false)
+})
+
+server.get("/pokemon/:tier/:slug", (req, res) => {
     const pkmn = req.params.slug
     const tier = req.params.tier
 
-    if (tier in battledex){
+    if (tier in localDex) {
 
-        if(pkmn in battledex[tier]){
-
-            let pokemon = battledex[tier][pkmn] 
-            
-            pokemon.types = dexdata[pokemon.slug].types
-            console.log(pokemon)
-            pokemon.stats = dexdata[pokemon.slug].stats
-            
-            return res.json({dogars: pokemon, status: 1})
+        if (pkmn in localDex[tier]) {
+            let pokemon = localDex[tier][pkmn]
+            return res.json({ dogars: pokemon, status: 1 })
         }
     }
-    return res.json({dogars: false, status:0})
+    return res.json({ dogars: false, status: 0 })
 
 })
 
-server.get("/:tier/all",(req,res)=>{
-    return {
-        dexdata:  dexdata
+server.get("pokemon/:tier", (req, res) => {
+    const tier = req.params.tier
+    if (tier in localDex) {
+        return res.json({ dogars: localDex[tier][req.params.tier], status: 1 })
     }
+    return res.json({ dogars: null, status: 0 })
 })
 
 server.listen(port, () => console.log(`Express started at port: ${port}`));
