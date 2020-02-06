63e67a5604cd007d27cbd5277fe173d1268c945b
testuser <joaovictor.clementino@gmail.com>
2020-02-06 06:56:41 -0300

    update website

---
+++ public/js/chunk-2d21ed46.7ce389d6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21ed46"],{d6db:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h1",[n._v("BREAK MY TEAM")])},c=[],l=t("2877"),r={},s=Object(l["a"])(r,u,c,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d21ed46.7ce389d6.js.map
