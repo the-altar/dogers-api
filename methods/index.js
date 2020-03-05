6722f4e43dcfc3b31b69481a4830a116e3ccd72b
testuser <joaovictor.clementino@gmail.com>
2020-03-05 10:21:33 -0300

    update pages

--- methods/index.js
+++ methods/index.js
@@ -2,6 +2,16 @@ exports.findPokemon = function (pokemon, pokedex){
     let pkmn = false
     if (pokemon in pokedex){
         pkmn = pokedex[pokemon]
+    } else {
+        return false
     }
-    return pkmn
+
+    return {
+        moves: pkmn.moves,
+        spreads: pkmn.spreads,
+        items: pkmn.items,
+        abilities: pkmn.abilities,
+        stats: pkmn.stats,
+        level: pkmn.level
+    }
 }
