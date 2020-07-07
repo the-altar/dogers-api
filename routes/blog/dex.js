c819adbe0af9362f72b5c9ce9d0eb326730c535a
JVSC <joaovictor.clementino@gmail.com>
2020-07-07 01:23:42 -0300

    update

--- routes/blog/dex.js
+++ routes/blog/dex.js
@@ -1,4 +1,5 @@
 const dexController = require("../../controllers/dex")
 module.exports = function(app){
     app.get('/dex/:tier', dexController.dexPage)
+    app.get('/dex', dexController.dexPage)
 }
