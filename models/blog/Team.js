13d2acc90db09c0956ec3b3381ff2098fba3c27b
JVSC <joaovictor.clementino@gmail.com>
2020-08-19 15:54:15 -0300

    add created time parameters

--- models/blog/Team.js
+++ models/blog/Team.js
@@ -6,7 +6,10 @@ const TeamSchema = Schema({
   tier: {type: String },
   trainer: {type: Number},
   raw: {type: String},
+  rentalCode: {type:String},
   teamMembers: [{name: String}]  
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
 module.exports = mongoose.model('Team', TeamSchema);
